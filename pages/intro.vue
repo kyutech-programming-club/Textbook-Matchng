c9c11c107e293eaba6ab54c0f0fb5e82623f5cd1
o-xian999 <onoyama1999910@gmail.com>
2020-10-11 17:31:53 +0900

    Make intro.vue

---
+++ pages/intro.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <section>
+      <h2>教科書を、次の世代へ</h2>
+      <n-link 
+        to="login"
+        class="button--green">Sign Up</n-link>
+      <a class="button--green">Sign In</a>
+    </section>
+
+    <section>
+      <h2>積んでる教科書は後輩に</h2>
+      <a>家に積んである教科書はTextbook-Matchingに投稿して、取引したい後輩と交渉しましょう</a>
+    </section>
+
+    <section>
+      <h1>同じ学部の後輩はその教科書を欲しがっているかも？</h1>
+      <a>学部・学科を登録すれば、大学内直属の後輩と繋がりやすくなります。</a>
+    </section>
+
+    <section>
+      <h1>取引は対面で</h1>
+      <a>ほげほげ</a>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
