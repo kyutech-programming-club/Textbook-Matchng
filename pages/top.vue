afd9b6fbf2ff9407f4e444342917b39a1133e6fd
o-xian999 <onoyama1999910@gmail.com>
2020-10-11 17:30:52 +0900

    Make top.vue

---
+++ pages/top.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <section>
+      <ul>
+        <li>電気宇宙2年の教科書あります！</li>
+        <li>想シスの教科書引取募集</li>
+        <li>1〜3年次数学の教科書あり、2000円</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
